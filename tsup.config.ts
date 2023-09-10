6ab8d68504ec2024472d786bc81d076e1498aeec
Luke Morales <lukemorales@live.com>
2023-09-10 15:28:42 -0300

    prepare ci an optimize build script

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,16 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   name: 'funkcia',
-  entry: ['src/index.ts'],
+  entry: [
+    'src/index.ts',
+    'src/array.ts',
+    'src/either.ts',
+    'src/functions.ts',
+    'src/number.ts',
+    'src/option.ts',
+    'src/predicate.ts',
+    'src/string.ts',
+  ],
   format: ['cjs', 'esm'],
   outDir: 'dist',
   clean: true,
