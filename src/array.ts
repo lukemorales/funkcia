350c50dad4f55f8b7d8f6799519907168d77183e
Luke Morales <lukemorales@live.com>
2023-09-10 22:13:10 -0300

    fix: simplify bundle setup (#3)
    * simplify bundle config
    
    * improve `head` and `tail` types and create `map` function

--- src/array.ts
+++ src/array.ts
@@ -9,10 +9,18 @@ export function last<A>(array: readonly A[]): O.Option<A> {
   return array.length ? O.some(array[array.length - 1]!) : O.none();
 }
 
-export function head<A>(array: readonly A[]): O.Option<A[]> {
-  return array.length ? O.some(array.slice(0, array.length - 1)) : O.none();
+export function head<A>(array: readonly A[]): O.Option<[A, ...A[]]> {
+  return array.length
+    ? O.some(array.slice(0, array.length - 1) as any)
+    : O.none();
 }
 
-export function tail<A>(array: readonly A[]): O.Option<A[]> {
-  return array.length ? O.some(array.slice(1)) : O.none();
+export function tail<A>(array: readonly A[]): O.Option<[...A[], A]> {
+  return array.length ? O.some(array.slice(1) as any) : O.none();
+}
+
+export function map<A, B>(
+  callback: (item: A) => B,
+): (array: readonly A[]) => B[] {
+  return (array) => array.map(callback);
 }
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,16 +3,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   name: 'funkcia',
-  entry: [
-    'src/index.ts',
-    'src/array.ts',
-    'src/either.ts',
-    'src/functions.ts',
-    'src/number.ts',
-    'src/option.ts',
-    'src/predicate.ts',
-    'src/string.ts',
-  ],
+  entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   outDir: 'dist',
   clean: true,
