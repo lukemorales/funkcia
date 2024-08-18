62d6149879716a0835803a5078e532f7338e4b42
Luke Morales <mail@lukemorales.io>
2024-08-18 16:51:12 -0300

    chore: reset implementation

--- src/_internals/types.ts
+++
@@ -1,5 +0,0 @@
-export type Nullable<T> = T | null | undefined;
-
-export type Mutable<T> = { -readonly [P in keyof T]: T[P] };
-
-export type Falsy = false | '' | 0 | 0n | null | undefined;
--- src/vitest-setup.ts
+++ src/vitest-setup.ts
@@ -1,1 +1,5 @@
-import './funkcia-matchers';
+/* eslint-disable import/no-extraneous-dependencies */
+import * as matchers from 'jest-extended';
+import { expect } from 'vitest';
+
+expect.extend(matchers);
